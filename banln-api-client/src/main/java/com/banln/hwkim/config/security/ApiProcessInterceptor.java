09469f61a03efda03a873bc4648d91fadf0c1f7a
yukikaze92 <yukikaze92@koscom.co.kr>
2021-11-08 15:50:51 +0900

    client 추가2

---
+++ banln-api-client/src/main/java/com/banln/hwkim/config/security/ApiProcessInterceptor.java
@@ -0,0 +1,6 @@
+package com.banln.hwkim.config.security;
+
+import org.springframework.web.servlet.AsyncHandlerInterceptor;
+
+public class ApiProcessInterceptor implements AsyncHandlerInterceptor {
+}
